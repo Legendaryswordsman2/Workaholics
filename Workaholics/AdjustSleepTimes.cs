c1d419c7d40839562abb3460c0e577ec653b708f
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-01-23 23:37:30 -0800

    Uploaded Mod

---
+++ Workaholics/AdjustSleepTimes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModLoaderInterfaces;
+
+namespace Workaholics
+{
+    public class AdjustSleepTimes : IAfterWorldLoad
+    {
+        public void AfterWorldLoad()
+        {
+            ServerManager.ServerSettings.Time.SleepTimeStart = 23.999f;
+            ServerManager.ServerSettings.Time.SleepTimeEnd = 0;
+            ServerManager.ServerSettings.Time.GuardShiftDayStart = ServerManager.ServerSettings.Time.SleepTimeEnd;
+            ServerManager.ServerSettings.Time.GuardShiftDayEnd = ServerManager.ServerSettings.Time.SleepTimeStart;
+            ServerManager.ServerSettings.Time.GuardShiftNightStart = ServerManager.ServerSettings.Time.SleepTimeEnd;
+            ServerManager.ServerSettings.Time.GuardShiftNightEnd = ServerManager.ServerSettings.Time.SleepTimeStart;
+        }
+    }
+}
